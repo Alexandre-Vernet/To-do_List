3cb802b04561968b3af586f0c543893288471557
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2020-12-01 21:39:08 +0100

    Création de AjoutTacheActivity

--- app/src/main/java/com/ynov/vernet/to_dolist/MainActivity.java
+++ app/src/main/java/com/ynov/vernet/to_dolist/MainActivity.java
@@ -1,14 +1,18 @@
 package com.ynov.vernet.to_dolist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +20,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listView = findViewById(R.id.listView);
+        floatingActionButton = findViewById(R.id.floatingActionButton);
 
+        // Afficher la liste des tâches
         ArrayAdapter<String> tableau = new ArrayAdapter<>(listView.getContext(), R.layout.element, R.id.textViewTache);
         for (int i = 0; i < 40; i++) {
             tableau.add("coucou " + i);
         }
         listView.setAdapter(tableau);
+
+        floatingActionButton.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(), AjoutTacheActivity.class));
+            finish();
+        });
     }
 }
