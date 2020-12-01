b18536b7384b8684ec917a2de62871df7da6a13a
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2020-12-01 21:29:52 +0100

    Ajout d'une case à cocher à côté des tâches

--- app/src/main/java/com/ynov/vernet/to_dolist/MainActivity.java
+++ app/src/main/java/com/ynov/vernet/to_dolist/MainActivity.java
@@ -1,14 +1,26 @@
 package com.ynov.vernet.to_dolist;
 
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+
+        ArrayAdapter<String> tableau = new ArrayAdapter<>(listView.getContext(), R.layout.element, R.id.textViewTache);
+        for (int i = 0; i < 40; i++) {
+            tableau.add("coucou " + i);
+        }
+        listView.setAdapter(tableau);
     }
 }
