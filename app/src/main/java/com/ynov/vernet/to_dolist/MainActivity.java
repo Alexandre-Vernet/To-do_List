3602e7bca4124ab1131a8672f12c401ca337f27e
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2020-12-03 13:58:17 +0100

    Ajout d'une Progressbar pendant le chargement des tâches

--- app/src/main/java/com/ynov/vernet/to_dolist/MainActivity.java
+++ app/src/main/java/com/ynov/vernet/to_dolist/MainActivity.java
@@ -3,19 +3,28 @@ package com.ynov.vernet.to_dolist;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 public class MainActivity extends AppCompatActivity {
 
+    ProgressBar progressBar;
     ListView listView;
     FloatingActionButton floatingActionButton;
     FirebaseFirestore db;
@@ -26,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         db = FirebaseFirestore.getInstance();
+        progressBar = findViewById(R.id.progressBar);
         listView = findViewById(R.id.listView);
         floatingActionButton = findViewById(R.id.floatingActionButton);
 
@@ -34,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
                 .get()
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
+                        progressBar.setVisibility(View.INVISIBLE);
+
                         // Afficher les tâches
                         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(listView.getContext(), R.layout.element, R.id.textViewTache);
                         for (QueryDocumentSnapshot document : task.getResult())
@@ -48,6 +60,13 @@ public class MainActivity extends AppCompatActivity {
                         Log.w("Erreur", "Erreur dans la récupération des tâches", task.getException());
                     }
                 });
+
+        // Au clic d'un élément
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            // Afficher un toast avec le nom du contact
+            String tache = (String) listView.getItemAtPosition(position);
+            Toast.makeText(MainActivity.this, tache, Toast.LENGTH_SHORT).show();
+        });
 
 
         // Ajouter une tâche
