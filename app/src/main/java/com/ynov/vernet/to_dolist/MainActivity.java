daea14e04f020ea77c316e3dff6838c3c5035d7b
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2021-02-17 16:04:13 +0100

    Create class Room

--- app/src/main/java/com/ynov/vernet/to_dolist/MainActivity.java
+++ app/src/main/java/com/ynov/vernet/to_dolist/MainActivity.java
@@ -30,7 +30,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,7 +43,6 @@ public class MainActivity extends AppCompatActivity {
 
     private int countTask = 0;
 
-    String room;
     String name;
 
     ArrayList<String> arrayListId;
@@ -106,25 +104,8 @@ public class MainActivity extends AppCompatActivity {
             alertDialog.setCancelable(false);
         }
 
-        // Check if user had room
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-        room = prefs.getString("room", null);
-
-        // if no room
-        if (room == null) {
-            // Generate code room
-            String characters = "1234567890AZERTYUIOPQSDFGHJKLMWXCVBN";
-            final Random random = new Random();
-            final StringBuilder sb = new StringBuilder(6);
-            for (int i = 0; i < 6; ++i)
-                sb.append(characters.charAt(random.nextInt(characters.length())));
-            room = sb.toString();
-
-            // Save it in memory
-            SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
-            editor.putString("room", room);
-            editor.apply();
-        }
+        // Get room
+        String room = new Room(this, this).getRoom();
 
         // Menu
         new Menu(this, this);
